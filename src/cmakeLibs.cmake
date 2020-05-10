22e4643ce01e76d97abeca3060b9c635ade98c77
Dave Knopp <davetrimstel@gmail.com>
2020-05-09 18:15:59 -0600

    src/CMakeLists.txt added comments on content

--- src/cmakeLibs.cmake
+++ src/cmakeLibs.cmake
@@ -1,17 +1,24 @@
 #
-
+# cmake commands for creating library targets
+#
 
 set(local_TARGET ${local_PREFIX})
 
 add_library(${local_TARGET} STATIC ${${local_PREFIX}_SOURCES})
 target_sources(${local_TARGET} PUBLIC ${${local_PREFIX}_INCLUDES})
 target_include_directories(${local_TARGET} PUBLIC ../)
-target_include_directories(${local_TARGET} PUBLIC /usr/include/eigen3)
 
 target_compile_features(${local_TARGET} PRIVATE cxx_std_11)
 
-install(FILES ${${local_PREFIX}_INCLUDES} DESTINATION include/litetpqz)
-install(TARGETS ${local_TARGET} ARCHIVE DESTINATION lib)
+install(
+	FILES ${${local_PREFIX}_INCLUDES}
+	DESTINATION include/${CMAKE_PROJECT_NAME}
+	)
+install(
+	TARGETS ${local_TARGET}
+	ARCHIVE
+	DESTINATION lib
+	)
 
 target_include_directories(${local_TARGET} PUBLIC ${EIGEN3_INCLUDE_DIRS})
 
