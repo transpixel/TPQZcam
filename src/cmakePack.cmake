b814d5daac32590637c6ac1bdffef6efd4ae31ca
Dave Knopp <davetrimstel@gmail.com>
2020-05-14 13:34:02 -0600

    CMakeLists added rudimentary debian packaging

---
+++ src/cmakePack.cmake
@@ -0,0 +1,15 @@
+
+# cmakePack.cmake
+
+
+# Specify various generic-package distribuation properties
+# set(CPACK_PACKAGE_NAME ${PROJECT_NAME}) # same as default
+set(CPACK_PACKAGE_VENDOR "Transpixel.org")
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION_TWEAK ${PROJECT_VERSION_TWEAK})
+
+
+include(cmakePackDEB.cmake)
+
---
+++ src/cmakePackDEB.cmake
@@ -0,0 +1,9 @@
+
+# Set package(Generator) specific characteristics
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Transpixel.org")
+
+#-#set(CPACK_DEBIAN_PACKAGE_DEPENDS
+#-#	"libc6 (>= 2.27), libgcc1 (>= 1:8), libstdc++6 (>=8.3)"
+#-#	)
+
+
