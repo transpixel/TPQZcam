bdd50dc272d891ed6969407663ea6bb9e346d6ce
Dave Knopp <davetrimstel@gmail.com>
2020-05-15 10:09:01 -0600

    src/CMakeLists.txt moved library install to main CMakeLists.txt from cmakeLibs.cmake

--- src/cmakeLibs.cmake
+++ src/cmakeLibs.cmake
@@ -6,10 +6,6 @@
 # set(local_LIBTARGET ${local_PREFIX})
 # add_library(${local_LIBTARGET} STATIC)
 
-message("local_NM: " ${local_NM})
-message("local_PREFIX: " ${local_PREFIX})
-message("local_LIBTARGET: " ${local_LIBTARGET})
-
 target_sources(${local_LIBTARGET} PRIVATE ${${local_PREFIX}_SOURCES})
 target_sources(${local_LIBTARGET} PUBLIC ${${local_PREFIX}_INCLUDES})
 target_include_directories(${local_LIBTARGET} PUBLIC ../)
@@ -20,11 +16,4 @@ install(
 	FILES ${${local_PREFIX}_INCLUDES}
 	DESTINATION include/${tpqzcam_LIBVERSION}/lib${local_NM}
 	)
-install(
-	TARGETS ${local_LIBTARGET}
-	ARCHIVE
-	DESTINATION lib/${tpqzcam_LIBVERSION}
-	)
-
-target_include_directories(${local_LIBTARGET} PUBLIC ${EIGEN3_INCLUDE_DIRS})
 
