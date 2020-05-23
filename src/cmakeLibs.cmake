eccdb3fddde843fbc76355bc1f3ab670ad0093a5
Dave Knopp <dave@stellacore.com>
2020-05-23 15:58:09 -0600

    CMake build with 3.10

---
+++ src/cmakeLibs.cmake
@@ -0,0 +1,21 @@
+#
+# cmake commands for creating library targets
+#
+
+target_sources_local(
+	${local_LIBTARGET}
+	PRIVATE
+		${${local_PREFIX}_SOURCES}
+	)
+
+target_sources_local(
+	${local_LIBTARGET}
+	PUBLIC
+		${${local_PREFIX}_INCLUDES}
+	)
+
+install(
+	FILES ${${local_PREFIX}_INCLUDES}
+	DESTINATION include/${tpqzcam_LIBVERSION}/lib${local_NM}
+	)
+
