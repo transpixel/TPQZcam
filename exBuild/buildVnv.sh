b44a97d44b49e9aa76c36efb6e4102091ff36c67
Dave Knopp <dave@stellacore.com>
2020-05-24 07:46:13 -0600

    exBuild/buildVnv.sh example script to build validation program with cmake find_package() import

---
+++ exBuild/buildVnv.sh
@@ -0,0 +1,30 @@
+workDir=/repos/TPQZcam
+instDir=${workDir}/tmpLocalInstall
+vnvDir=${workDir}/vnv
+buildDir=${workDir}/tmpVnv
+
+if ! cd ${vnvDir} ; then
+	echo "cd Failure to ${vnvDir}"
+else
+	# cleanup vnv application build directory
+	rm -rf ${buildDir}
+	mkdir ${buildDir}
+
+	# build vnv application
+	cd ${buildDir}
+	cmake \
+		-DCMAKE_PREFIX_PATH=${instDir}/ \
+		${vnvDir} \
+		&& cmake --build . \
+		;
+
+	if \
+		./testBuild;
+	then
+		echo "Success"
+	else
+		echo "FAILED build"
+	fi
+
+fi
+
