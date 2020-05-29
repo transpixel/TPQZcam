b9d8d5fcf01cb1c961567e7f05b1a1669d14f313
Dave Knopp <dave@stellacore.com>
2020-05-29 11:38:47 -0600

    vnv/CMake added message output on found versions

--- vnv/testBuild.cpp
+++ vnv/testBuild.cpp
@@ -13,11 +13,12 @@ main
 	)
 {
 	std::cout << argv[0] << std::endl;
-
 	std::cout << dat::infoString(1.25, "1.25") << std::endl;
 
 	ga::Vector const vec{ 1., 2., 3. };
-	std::cout << dat::infoString(ga::vZero, "ga::vZero") << std::endl;
+	ga::Vector const perp{ ga::anyPerpendicularDir(vec) };
+	std::cout << dat::infoString(vec, "vec") << std::endl;
+	std::cout << dat::infoString(perp, "perp") << std::endl;
 
 	cam::Camera const cam(2.5, dat::Extents{ 100u, 200u });
 	std::cout << dat::infoString(cam, "cam") << std::endl;
